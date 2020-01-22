1a7051f3cb5c2e60addb320fffda9fe450d128e8
Larissa <lani.macedo@gmail.com>
2020-01-21 23:25:02 -0300

    novos ajustes

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
 import * as restify from 'restify'
 
-console.log('teste')
+const server = restify.createServer({
+    name: 'meat-api',
+    version: '1.0.0'
+})
+
+server.get('/hello', (req, resp, next) => {
+    resp.json({message: 'Oi'})
+    return next()
+})
+
+server.listen((3000), () => {
+    console.log('API is running on http://localhost:3000')
+})
